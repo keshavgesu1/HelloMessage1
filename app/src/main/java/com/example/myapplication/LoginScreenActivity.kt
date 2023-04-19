4ccbc86af5c449cc9bacaf24140eef745a1bf2f3
keshav <keshavraj4546@gmail.com>
2023-04-19 12:15:27 +0530

    add push to new activtyClass

--- app/src/main/java/com/example/myapplication/LoginScreenActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginScreenActivity.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class LoginScreenActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_first)
+        var btn = findViewById<Button>(R.id.buttonn)
+        btn.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
