d7e1c2d6bc61caed2feec67961148195d12fa2b0
keshav <keshavraj4546@gmail.com>
2023-04-19 15:05:47 +0530

    add toad message on tap of Button

---
+++ app/src/main/java/com/example/myapplication/CongratulationsScreen.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CongratulationsScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_congratulations_screen)
+    }
+}
--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -1,11 +1,49 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+//        var btn = findViewById<Button>(R.id.buttonSignIn)
+//        btn.setOnClickListener {
+//            val intent = Intent(this,CongratulationsScreen::class.java)
+//            startActivity(intent)
+//        }
+
     }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    fun GoToLoginScreen(view: View) {
+        val n = findViewById<EditText>(R.id.nameTextField)
+        var name = n.text
+        Toast.makeText(this,"Congratulations...$name", Toast.LENGTH_SHORT).show()
+        /*
+        val intent = Intent(this,CongratulationsScreen::class.java)
+        startActivity(intent)
+        */
+    }
+
 }
